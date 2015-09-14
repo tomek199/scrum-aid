67d29c1bdc44e6b7e05547f8d72f2c2ab30b235e
tomek199 <t.morek@gmail.com>
2015-09-14 21:37:05 +0200

    Add [sprint_length] field to Project

--- app/models/project.rb
+++ app/models/project.rb
@@ -5,13 +5,14 @@ class Project
   field :description, type: String
   field :owner_id, type: BSON::ObjectId
   field :owner_username, type: String
+  field :sprint_length, type: Integer, default: 14
 
   has_and_belongs_to_many :users
   has_many :roles, dependent: :destroy
   has_many :user_roles, dependent: :destroy
   has_many :sprints, dependent: :destroy
 
-  validates_presence_of :name, :owner_id, :owner_username
+  validates_presence_of :name, :owner_id, :owner_username, :sprint_length
 
   def add_default_roles
     self.roles << Role.create(name: ProjectsHelper::PRODUCT_OWNER, removable: false, editable: false)
--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -2,10 +2,12 @@ require 'rails_helper'
 
 RSpec.describe Project, type: :model do
   it {is_expected.to have_fields(:name, :description, :owner_id)}
+  it {is_expected.to have_field(:sprint_length).of_type(Integer).with_default_value_of(14)}
   it {is_expected.to have_and_belong_to_many(:users)}
   it {is_expected.to validate_presence_of(:name)}
   it {is_expected.to validate_presence_of(:owner_id)}
   it {is_expected.to validate_presence_of(:owner_username)}
+  it {is_expected.to validate_presence_of(:sprint_length)}
   it {is_expected.to have_many(:roles)}
   it {is_expected.to have_many(:user_roles)}
   it {is_expected.to have_many(:sprints)}
